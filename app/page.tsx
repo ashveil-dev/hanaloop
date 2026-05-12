c2f2db732a911454915d3310aba06033cba3edad
ashveil <ashveil.dev@gmail.com>
2026-05-13 06:16:33 +0900

    @Feat 백엔드에서 사용할 수 있는 Alias 추가하기

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { fetchCountries } from "@/lib/backend/api";
+import { fetchCountries } from "@/lib/server/api";
 
 export default async function Home() {
   const posts = await fetchCountries();

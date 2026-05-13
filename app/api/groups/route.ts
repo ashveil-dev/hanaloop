c077c30c66ffd6748241c5ab5ee1acd238c9b08a
ashveil <ashveil.dev@gmail.com>
2026-05-13 11:16:27 +0900

    @Docs 그룹 목록을 요청하는 API의 Swagger 문서 작성하기

--- app/api/groups/route.ts
+++ app/api/groups/route.ts
@@ -1,5 +1,33 @@
 import { getGroups } from "@/lib/server/services/groups/getGroups";
 
+/**
+ * @swagger
+ * /api/groups:
+ *   get:
+ *     summary: 그룹 목록 조회
+ *     responses:
+ *       200:
+ *          description: 성공
+ *          content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     example: 2
+ *                   name:
+ *                     type: string
+ *                     example: groupName
+ *                   parent_id:
+ *                     type: integer
+ *                     example : 1
+ *                   created_at:
+ *                     type: string
+ *                     format: date-time
+ */
 export async function GET() {
   return Response.json(
     await getGroups()

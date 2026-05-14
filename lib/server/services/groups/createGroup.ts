b69d087a15f45dce230c67a6c55e2ade8b8862ee
ashveil <ashveil.dev@gmail.com>
2026-05-14 12:06:55 +0900

    @Refactor Service 에러 정리하기

--- lib/server/services/groups/createGroup.ts
+++ lib/server/services/groups/createGroup.ts
@@ -1,31 +1,19 @@
 import { db } from "@server/db"
-import { z } from "zod";
 import { GroupsTable } from "@server/db/schema/groups";
 
 type createGroupParameterType = {
-    name : string,
-    parentId : number | null
+    name: string,
+    parentId: number | null
 }
 
 export async function createGroup({
     name,
     parentId
 }: createGroupParameterType) {
-    const group: typeof GroupsTable.$inferInsert = {
-        name,
-        parentId
-    };
+    const result = await db
+        .insert(GroupsTable)
+        .values({ name, parentId })
+        .returning();
 
-    try {
-        const result = await db.insert(GroupsTable).values(group).returning();
-        return Response.json(result[0])
-    } catch(e) {
-        return Response.json(
-        {
-            message : "그룹 생성을 실패하였습니다."
-        }, {
-            status : 500
-        })
-    }
-
+    return result;
 }
--- lib/server/services/groups/getGroups.ts
+++ lib/server/services/groups/getGroups.ts
@@ -2,7 +2,9 @@ import { db } from "@server/db"
 import { GroupsTable } from "@server/db/schema/groups";
 
 export async function getGroups() {
-    const result = await db.select().from(GroupsTable);
+    const result = await db
+        .select()
+        .from(GroupsTable);
 
     return result;
 }

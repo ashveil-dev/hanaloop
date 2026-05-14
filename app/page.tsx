3e8296499180044e2e20f33d25a9388ab9f620a7
ashveil <ashveil.dev@gmail.com>
2026-05-14 09:59:54 +0900

    @Refactor 사용되지 않는 코드 제거하기

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,4 @@
-"use client";
-
-import { useEffect } from "react";
-
 export default function Home() {
-  useEffect(() => {
-    fetch("/api/groups")
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  }, []);
-  
-
   return (
     <div>
       Home
--- lib/server/api.ts
+++ lib/server/api.ts
@@ -11,19 +11,3 @@ const posts: Post[] = []
 const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
 const jitter = () => 200 + Math.random() * 600; // 로딩 중일때 어떻게 UI를 표현할 것인가?
 const maybeFail = () => Math.random() < 0.15; // 실패했을 경우 어떻게 처리할 것인가?
-
-export async function createOrUpdatePost(p: Omit<Post, "id"> & { id?: string }) {
-  await delay(jitter());
-  if (maybeFail()) throw new Error("Save failed");
-
-  const result = await db.query(`SELECT * FROM posts`);
-  let posts = result.rows;
-
-  if (p.id) {
-    posts = posts.map(x => x.id === p.id ? (p as Post) : x);
-    return p as Post;
-  }
-  const created = { ...p, id: crypto.randomUUID() };
-  posts = [...posts, created];
-  return created;
-}

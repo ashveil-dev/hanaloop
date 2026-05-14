59a01631de98e5bddfc4c1256fa9e9ca12d699d7
ashveil <ashveil.dev@gmail.com>
2026-05-14 20:11:57 +0900

    @Feat 기존의 주석 방식이 아닌 Zod의 openapi를 통해 문서화하기

--- app/api-docs/page.tsx
+++ app/api-docs/page.tsx
@@ -1,6 +1,10 @@
+"use client"
+
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
+import { document } from "@/lib/server/zod/openApi";
+
 
 export default function ApiDocsPage() {
-  return <SwaggerUI url="/api/swagger" />;
+  return <SwaggerUI spec={document} />;
 }

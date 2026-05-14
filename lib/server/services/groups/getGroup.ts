d2547fa49dc8d255a11148a18643530bba3d7b05
ashveil <ashveil.dev@gmail.com>
2026-05-14 18:26:51 +0900

    @Feat getGroup API 작성하기

---
+++ lib/server/services/groups/getGroup.ts
@@ -0,0 +1,26 @@
+import { db } from "@server/db"
+import { eq } from "drizzle-orm";
+import { ApiError } from "@/lib/server/errors/ApiError";
+import { GroupsTable } from "@/lib/server/db/schema/groups";
+
+type getGroupParameterType = {
+    id: number
+}
+
+export async function getGroup({
+    id
+}: getGroupParameterType) {
+    const result = await db
+        .select()
+        .from(GroupsTable)
+        .where(eq(GroupsTable.id, id))
+
+    if (result.length === 0) {
+        throw new ApiError({
+            status: 400,
+            message: "The selected group does not exist."
+        })
+    }
+
+    return result;
+}

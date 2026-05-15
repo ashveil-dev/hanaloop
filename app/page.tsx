35b97fca322bf3923d1092314e252b9fef62db9f
ashveil <ashveil.dev@gmail.com>
2026-05-15 10:27:55 +0900

    @Design 반응형 디자인 추가하기

--- app/page.tsx
+++ app/page.tsx
@@ -4,24 +4,31 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="w-full h-full">
-      <header className="flex justify-between p-6">
+      <header className="flex p-4 items-center justify-between md:p-6">
         <div className="flex gap-4 items-center">
           <Link href="/">
             <h3 className="flex gap-2 items-center">
-              <Image src="/icons/logo.png" width={50} height={50} alt="" />
-              <span className="text-2xl">하나 대시보드</span>
+              <Image src="/icons/logo.png" width={50} height={50} alt="logo" />
+              <span className="text-lg md:text-2xl">하나 대시보드</span>
             </h3>
           </Link>
-          <h6 className="text-sm">탄소 관리 플랫폼 </h6>
+          <h6 className="hidden md:block md:text-sm">탄소 관리 플랫폼 </h6>
         </div>
         <div className="flex gap-4 items-center">
-          <Link href="https://www.hanaloop.com/" target="_blank">
-            <Image src="/icons/hanaloop.png" width={70} height={50} alt="" />
+          <Link href="https://www.hanaloop.com/" target="_blank" className="hidden md:block">
+            <Image src="/icons/hanaloop.png" width={70} height={50} alt="hanaloop website" />
           </Link>
-          <Link href="https://www.hana.eco/">
-            <Image src="/icons/hanaeco.png" width={50} height={50} alt="" />
+          <Link href="https://www.hana.eco/" className="hidden md:block">
+            <Image src="/icons/hanaeco.png" width={50} height={50} alt="hana eco website" />
           </Link>
+          <div className="md:hidden">
+            <button className="cursor-pointer">
+              <Image src="/images/menu.png" width={30} height={30} alt="Menu" />
+            </button>
+          </div>
         </div>
+
+
       </header>
       <div id="sidebar" className="w-1/3 min-w-[300] h-full ">
 

de32e1bd34848e405cf7acd211a0659ffab6d900
ashveil <ashveil.dev@gmail.com>
2026-05-17 17:48:37 +0900

    @Feat editEmissionRecord API 만들기

---
+++ lib/client/api/editEmissionRecord.ts
@@ -0,0 +1,36 @@
+import type { EmissionRecord } from "@/lib/client/types/emissionRecords";
+
+type editEmissionRecord = {
+    id: number,
+    groupId: number,
+    scopeType: "SCOPE1" | "SCOPE2" | "SCOPE3",
+    amount: number,
+    unit: string,
+    recordedAt: string
+}
+
+export async function editEmissionRecord({
+    id,
+    groupId,
+    scopeType,
+    amount,
+    unit,
+    recordedAt
+}: editEmissionRecord): Promise<EmissionRecord> {
+    const res = await fetch("/api/emission-records", {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            id,
+            groupId,
+            scopeType,
+            amount,
+            unit,
+            recordedAt
+        }),
+    });
+
+    return res.json();
+}
